6c186c819548e17b8087971b736eeb26c6bbe1a6
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2024-08-25 17:42:16 -0500

    Add JavaScript and CSS

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function convert(){
-    const inputValue = document.getElementById=("userInput").value;
-    const unit = document.getElementById("unit").value;
+    const inputValue = parseFloat(document.getElementById=("userInput").value);
+    const unit = document.getElementById=("unit").value;
     const milesToKm = unit === "milesToKm";
     let result = 0;
     if(milesToKm === true){
         result = inputValue * 1.60934;
+
     }
     else{
-        result = inputvalue / 1.60934;
+        result = inputValue / 1.60934;
+
     }
     const resultString = inputValue + "miles are "+ result + " Km";
     console.log(resultString);
-    const resultElement = document.getElementById("resultElement");
+    const resultElement = document.getElementById=("resultElement");
     resultElement.innerHTML = resultString;
 
 }
